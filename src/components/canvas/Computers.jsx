afddf35b18c4111bfbbaaaa43ea61fdaef2f9c75
Felipe <felipe.silva@kukac.com.br>
2024-01-09 19:10:32 -0300

    feat: navbar & welcome message

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -29,7 +29,7 @@ const ComputerCanvas = () => {
         <OrbitControls 
         enableZoom={false}
         maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 4}
         />
       </Suspense>
     </Canvas>
