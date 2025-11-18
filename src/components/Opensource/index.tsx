218384d9974cdb2d870c05c40a0ee223a7e1d4b1
Felipe Ramalho <feliperamalhoy@gmail.com>
2025-11-18 08:20:40 -0300

    fix node version

--- src/components/Opensource/index.tsx
+++ src/components/Opensource/index.tsx
@@ -1,12 +1,15 @@
+import { ArrowUpRight } from "lucide-react";
 
 type Contribution = {
   link: string;
+  project: string,
 }
 export function Opensource() {
 
   const contributions: Contribution[] = [
     {
-      link: "https://github.com/usekaneo/kaneo/pull/238"
+      link: "https://github.com/usekaneo/kaneo/pulls?q=is%3Apr+is%3Aclosed+author%3AFelipe32R",
+      project: "kaneo",
     }
   ]
 
@@ -14,8 +17,10 @@ export function Opensource() {
     <div className="flex flex-col gap-2 w-full ">
       <ul>
         {contributions.map((contribution) =>
-          <li>
-            <a href={contribution.link} target="_blank" className="hover:text-amber-100" >{contribution.link}</a>
+          <li >
+            <a href={contribution.link} target="_blank" className="hover:text-amber-100 border border-amber-100 max-w-20 p-1.5 text-center flex gap-2" >{contribution.project}
+              <ArrowUpRight size={22} />
+            </a>
           </li>
         )}
       </ul>
